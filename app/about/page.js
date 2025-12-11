aa96ce447499fd060baec355b647fcc98a4e3197
sirinezribi1 <zribisirine339@gmail.com>
2025-12-11 20:07:22 +0100

    my-next-app

---
+++ app/about/page.js
@@ -0,0 +1,16 @@
+import styles from "./About.module.css"
+const page = () => {
+  return (
+    <div clasName={styles.container}> About page
+
+
+    <div className="bg-blue shadow p-4 rounded">
+
+    </div>
+    </div>
+  )
+
+
+}
+
+export default page
