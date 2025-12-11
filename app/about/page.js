8926dca91d975ed2c45d1832dab543c99ee3698c
sirinezribi1 <zribisirine339@gmail.com>
2025-12-11 20:27:56 +0100

    remove module css file

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
-import styles from "./About.module.css"
+
 const page = () => {
   return (
-    <div clasName={styles.container}> About page
+    <div> About page
 
 
     <div className="bg-blue shadow p-4 rounded">
